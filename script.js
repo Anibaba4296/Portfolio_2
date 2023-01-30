0e80c34f0e1cc789db11e367a37960e48b176582
Anibaba4296 <regiusdeveloperclub@gmail.com>
2023-01-30 13:12:33 +0530

    project_details.1

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function Details1() {
-    document.getElementById("prjdtls").innerHTML = "This is function 1";
+    const variable = document.getElementById("prjdtls");
+    variable.innerHTML = "<h3>Apex Sports club website</h3><br> <ul><li>Used technologies are HTML, CSS and JavaScript</li><li>It is static website based on Apex sports club</li><li>It includes attributes : Homepage, about club, gallery, registration, contact.</li><li><a href='https://github.com/Anibaba4296/SportsClubSite.com'>GitHub repository</li></ul>";
+    variable.style.color = "aqua";
+    variable.style.paddingLeft = "20px"
+
 }
 
 function Details2() {
-    document.getElementById("prjdtls").innerHTML = "This is function 2";
+    const variable = document.getElementById("prjdtls");
+    variable.innerHTML = "<h3>Online Fir System</h3><br> <ul><li>Used technology used is Pthon</li><li>It is static website based on Apex sports club</li><li>It includes attributes : Homepage, about club, gallery, registration, contact.</li><li><a href='https://github.com/Anibaba4296/SportsClubSite.com'>GitHub repository</li></ul>";
+    variable.style.color = "aqua";
+    variable.style.paddingLeft = "20px"
 }
 
 function Details3() {
