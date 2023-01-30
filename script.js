a2f68a7077257f5a3f79b2fc8c37f3e518599501
Anibaba4296 <regiusdeveloperclub@gmail.com>
2023-01-30 12:18:07 +0530

    Project deatils added

---
+++ script.js
@@ -0,0 +1,11 @@
+function Details1() {
+    document.getElementById("prjdtls").innerHTML = "This is function 1";
+}
+
+function Details2() {
+    document.getElementById("prjdtls").innerHTML = "This is function 2";
+}
+
+function Details3() {
+    document.getElementById("prjdtls").innerHTML = "This is function 3";
+}
