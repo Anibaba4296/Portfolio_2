55f4e15cf695e367a73cb2b40735ac070b78e8a4
Anibaba4296 <regiusdeveloperclub@gmail.com>
2023-01-31 13:26:08 +0530

    project details udpdate 1

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ function Details1() {
 
 function Details2() {
     const variable = document.getElementById("prjdtls");
-    variable.innerHTML = "<h3>Online Fir System</h3><br> <ul><li>Used technology used is Pthon</li><li>It is static website based on Apex sports club</li><li>It includes attributes : Homepage, about club, gallery, registration, contact.</li><li><a href='https://github.com/Anibaba4296/SportsClubSite.com'>GitHub repository</li></ul>";
-    variable.style.color = "aqua";
+    variable.innerHTML = "<h3>Online Fir System</h3><br> <ul><li>Used technology used is Python and database.</li><li>It was a team project headed by me.</li><li>User-interface is designed using Python and records are stored at backend in a database.</li><li><a href='https://github.com/Anibaba4296/onlineFirSystem'>GitHub repository</li></ul>";
+    variable.style.color = "pink";
     variable.style.paddingLeft = "20px"
 }
 
 function Details3() {
-    document.getElementById("prjdtls").innerHTML = "This is function 3";
+    const variable = document.getElementById("prjdtls");
+    variable.innerHTML = "<h3>Demo shopping site</h3><br> <ul><li>Used technologies used are HTML, CSS and JavaScript.</li><li>It is a individual project.</li><li><a href='https://github.com/Anibaba4296/Shopping_site.anibaba'>GitHub repository</li></ul>";
+    variable.style.color = "indianred";
+    variable.style.paddingLeft = "20px"
 }
